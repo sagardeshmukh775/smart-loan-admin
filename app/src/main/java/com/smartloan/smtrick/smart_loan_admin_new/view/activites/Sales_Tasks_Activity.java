3110fe88aac64d15d40516d657c9e1fa68fe8799
sagar <sagardeshmukh447@gmail.com>
2019-12-11 14:18:27 +0530

    updated on 11.12.2019 at 2.18

---
+++ app/src/main/java/com/smartloan/smtrick/smart_loan_admin_new/view/activites/Sales_Tasks_Activity.java
@@ -0,0 +1,15 @@
+package com.smartloan.smtrick.smart_loan_admin_new.view.activites;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.smartloan.smtrick.smart_loan_admin_new.R;
+
+public class Sales_Tasks_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sales__tasks_);
+    }
+}
--- app/src/main/java/com/smartloan/smtrick/smart_loan_admin_new/view/activites/Sales_View_Lead_Details_Activity.java
+++ app/src/main/java/com/smartloan/smtrick/smart_loan_admin_new/view/activites/Sales_View_Lead_Details_Activity.java
@@ -2,13 +2,19 @@ package com.smartloan.smtrick.smart_loan_admin_new.view.activites;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.smartloan.smtrick.smart_loan_admin_new.R;
 
 public class Sales_View_Lead_Details_Activity extends AppCompatActivity {
 
-
+    CardView cardLabel, cardFolloe_up, cardNotes, cardAppointment, cardDocuments;
+    ImageView imgCall, imgMessage, imgEmail, imgNavigate, imgProfile;
+    TextView TXTusername;
 
     @Override
     public boolean onSupportNavigateUp() {
@@ -24,5 +30,26 @@ public class Sales_View_Lead_Details_Activity extends AppCompatActivity {
         setSupportActionBar((Toolbar) findViewById(R.id.toolbar1));
         assert getSupportActionBar() != null;   //null check
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        cardLabel = (CardView) findViewById(R.id.card_view_labels);
+        cardFolloe_up = (CardView) findViewById(R.id.card_view_foloup_task);
+        cardNotes = (CardView) findViewById(R.id.card_view_notes);
+        cardAppointment = (CardView) findViewById(R.id.card_view_appointment);
+        cardDocuments = (CardView) findViewById(R.id.card_view_documents);
+
+        imgCall = (ImageView) findViewById(R.id.imgCall);
+        imgMessage = (ImageView) findViewById(R.id.imgMessage);
+        imgEmail = (ImageView) findViewById(R.id.imgEmail);
+        imgNavigate = (ImageView) findViewById(R.id.imgNavigate);
+        imgProfile = (ImageView) findViewById(R.id.imgProfile);
+
+        TXTusername = (TextView) findViewById(R.id.txtUsername);
+
+        cardLabel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
