27f97241ccce8ef09545764e4f0d4021f8bac543
sagar <sagardeshmukh447@gmail.com>
2019-12-11 17:22:33 +0530

    updated on 11.12.2019 at 5.22

--- app/src/main/java/com/smartloan/smtrick/smart_loan_admin_new/view/activites/Sales_Tasks_Activity.java
+++ app/src/main/java/com/smartloan/smtrick/smart_loan_admin_new/view/activites/Sales_Tasks_Activity.java
@@ -1,15 +1,99 @@
 package com.smartloan.smtrick.smart_loan_admin_new.view.activites;
 
+import android.app.Fragment;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.smartloan.smtrick.smart_loan_admin_new.R;
+import com.smartloan.smtrick.smart_loan_admin_new.constants.Constant;
+import com.smartloan.smtrick.smart_loan_admin_new.models.LeedsModel;
+import com.smartloan.smtrick.smart_loan_admin_new.view.fragements.View_Sales_Received_Lead_Details_Fragment;
 
 public class Sales_Tasks_Activity extends AppCompatActivity {
+
+    LeedsModel leedsModel;
+    private ActionBar toolbar;
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sales__tasks_);
+
+
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar1));
+        assert getSupportActionBar() != null;   //null check
+        this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//        toolbar = getSupportActionBar();
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+//        toolbar.setTitle("Shop");
+
+        leedsModel = (LeedsModel) getIntent().getSerializableExtra(Constant.LEED_MODEL);
+        Intent intent = getIntent();
+        String task = intent.getStringExtra("Task");
+
+
+        if (task.equalsIgnoreCase("Label")) {
+
+            Bundle bundle = new Bundle();
+            bundle.putSerializable(Constant.LEED_MODEL, leedsModel);// Put anything what you want
+            View_Sales_Received_Lead_Details_Fragment fragment2 = new View_Sales_Received_Lead_Details_Fragment();
+            fragment2.setArguments(bundle);
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.frame_container, fragment2);
+            ft.commit();
+
+        } else if (task.equalsIgnoreCase("FallowUp")) {
+
+        } else if (task.equalsIgnoreCase("Notes")) {
+
+        } else if (task.equalsIgnoreCase("Appointment")) {
+
+        } else if (task.equalsIgnoreCase("Docs")) {
+
+        }
+
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment;
+            switch (item.getItemId()) {
+                case R.id.navigation_shop:
+                    toolbar.setTitle("Leeds");
+                    return true;
+                case R.id.navigation_gifts:
+                    toolbar.setTitle("Customers");
+                    return true;
+                case R.id.navigation_cart:
+                    toolbar.setTitle("Labels");
+                    return true;
+                case R.id.navigation_profile:
+                    toolbar.setTitle("Tasks");
+                    return true;
+                case R.id.navigation_business:
+                    toolbar.setTitle("My Business");
+                    return true;
+            }
+            return false;
+        }
+    };
 }
--- app/src/main/java/com/smartloan/smtrick/smart_loan_admin_new/view/activites/Sales_View_Lead_Details_Activity.java
+++ app/src/main/java/com/smartloan/smtrick/smart_loan_admin_new/view/activites/Sales_View_Lead_Details_Activity.java
@@ -1,5 +1,6 @@
 package com.smartloan.smtrick.smart_loan_admin_new.view.activites;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
@@ -9,12 +10,18 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.smartloan.smtrick.smart_loan_admin_new.R;
+import com.smartloan.smtrick.smart_loan_admin_new.constants.Constant;
+import com.smartloan.smtrick.smart_loan_admin_new.models.LeedsModel;
 
-public class Sales_View_Lead_Details_Activity extends AppCompatActivity {
+import static com.smartloan.smtrick.smart_loan_admin_new.constants.Constant.LEED_MODEL;
 
-    CardView cardLabel, cardFolloe_up, cardNotes, cardAppointment, cardDocuments;
+public class Sales_View_Lead_Details_Activity extends AppCompatActivity implements View.OnClickListener {
+
+    CardView cardLabel, cardFollow_up, cardNotes, cardAppointment, cardDocuments;
     ImageView imgCall, imgMessage, imgEmail, imgNavigate, imgProfile;
     TextView TXTusername;
+
+    LeedsModel leedsModel;
 
     @Override
     public boolean onSupportNavigateUp() {
@@ -31,8 +38,10 @@ public class Sales_View_Lead_Details_Activity extends AppCompatActivity {
         assert getSupportActionBar() != null;   //null check
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        leedsModel = (LeedsModel) getIntent().getSerializableExtra(Constant.LEED_MODEL);
+
         cardLabel = (CardView) findViewById(R.id.card_view_labels);
-        cardFolloe_up = (CardView) findViewById(R.id.card_view_foloup_task);
+        cardFollow_up = (CardView) findViewById(R.id.card_view_foloup_task);
         cardNotes = (CardView) findViewById(R.id.card_view_notes);
         cardAppointment = (CardView) findViewById(R.id.card_view_appointment);
         cardDocuments = (CardView) findViewById(R.id.card_view_documents);
@@ -44,12 +53,49 @@ public class Sales_View_Lead_Details_Activity extends AppCompatActivity {
         imgProfile = (ImageView) findViewById(R.id.imgProfile);
 
         TXTusername = (TextView) findViewById(R.id.txtUsername);
+        TXTusername.setText(leedsModel.getCustomerName());
 
-        cardLabel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        cardLabel.setOnClickListener(this);
+        cardFollow_up.setOnClickListener(this);
+        cardNotes.setOnClickListener(this);
+        cardAppointment.setOnClickListener(this);
+        cardDocuments.setOnClickListener(this);
+        imgCall.setOnClickListener(this);
+        imgMessage.setOnClickListener(this);
+        imgEmail.setOnClickListener(this);
+        imgNavigate.setOnClickListener(this);
+        imgProfile.setOnClickListener(this);
+    }
 
-            }
-        });
+    @Override
+    public void onClick(View v) {
+        if (v == cardLabel){
+            Intent intent = new Intent(Sales_View_Lead_Details_Activity.this,Sales_Tasks_Activity.class);
+            intent.putExtra("Task","Label");
+            intent.putExtra(LEED_MODEL, leedsModel);
+            startActivity(intent);
+        }else   if (v == cardFollow_up){
+            Intent intent = new Intent(Sales_View_Lead_Details_Activity.this,Sales_Tasks_Activity.class);
+            intent.putExtra("Task","FallowUp");
+            intent.putExtra(LEED_MODEL, leedsModel);
+            startActivity(intent);
+        }else   if (v == cardNotes){
+            Intent intent = new Intent(Sales_View_Lead_Details_Activity.this,Sales_Tasks_Activity.class);
+            intent.putExtra("Task","Notes");
+            intent.putExtra(LEED_MODEL, leedsModel);
+            startActivity(intent);
+        }else   if (v == cardAppointment){
+            Intent intent = new Intent(Sales_View_Lead_Details_Activity.this,Sales_Tasks_Activity.class);
+            intent.putExtra("Task","Appointment");
+            intent.putExtra(LEED_MODEL, leedsModel);
+            startActivity(intent);
+        }else   if (v == cardDocuments){
+            Intent intent = new Intent(Sales_View_Lead_Details_Activity.this,Sales_Tasks_Activity.class);
+            intent.putExtra("Task","Docs");
+            intent.putExtra(LEED_MODEL, leedsModel);
+            startActivity(intent);
+        }else   if (v == imgCall){
+
+        }
     }
 }
