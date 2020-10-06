ef6b9b5fbfd86f04862573ef3f36eb3bb060b758
sagar <sagardeshmukh447@gmail.com>
2020-10-06 15:55:18 +0530

    updated on 06.10.2020 at 3.55

--- app/src/main/java/com/smartloan/smtrick/smart_loan_admin_new/view/fragements/Sales_Fragment_Checklist.java
+++ app/src/main/java/com/smartloan/smtrick/smart_loan_admin_new/view/fragements/Sales_Fragment_Checklist.java
@@ -25,12 +25,14 @@ import com.smartloan.smtrick.smart_loan_admin_new.view.adapters.Tab_Adapter;
 
 import java.util.ArrayList;
 
-public class Sales_Fragment_Checklist extends Fragment implements AdapterView.OnItemSelectedListener {
+public class Sales_Fragment_Checklist extends Fragment implements AdapterView.OnItemSelectedListener,
+        OnCheckedClickListener {
 
     // NOTE: Removed Some unwanted Boiler Plate Codes
     private OnFragmentInteractionListener mListener;
 
-    public Sales_Fragment_Checklist() {}
+    public Sales_Fragment_Checklist() {
+    }
 
 
     RecyclerView recycleChecklist;
@@ -45,9 +47,9 @@ public class Sales_Fragment_Checklist extends Fragment implements AdapterView.On
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View view= inflater.inflate(R.layout.fragment_checklist, container, false);
+        View view = inflater.inflate(R.layout.fragment_checklist, container, false);
 
         // NOTE : We are calling the onFragmentInteraction() declared in the MainActivity
         // ie we are sending "Fragment 1" as title parameter when fragment1 is activated
@@ -62,12 +64,15 @@ public class Sales_Fragment_Checklist extends Fragment implements AdapterView.On
         }
 
         checklist = new ArrayList<>();
-        checklist =  leedsModel.getChecklist();
-        recycleChecklist = (RecyclerView) view.findViewById(R.id.recycler_view_users);
-        recycleChecklist.setHasFixedSize(true);
-        recycleChecklist.setLayoutManager(new LinearLayoutManager(getContext()));
-        checkListAdapter = new SalesChecklistAdapter(getContext(),checklist,(OnCheckedClickListener) Sales_Fragment_Checklist.this);
-        recycleChecklist.setAdapter(checkListAdapter);
+        checklist = leedsModel.getChecklist();
+
+        if (checklist != null) {
+            recycleChecklist = (RecyclerView) view.findViewById(R.id.recycler_view_users);
+            recycleChecklist.setHasFixedSize(true);
+            recycleChecklist.setLayoutManager(new LinearLayoutManager(getContext()));
+            checkListAdapter = new SalesChecklistAdapter(getContext(), checklist, (OnCheckedClickListener) Sales_Fragment_Checklist.this);
+            recycleChecklist.setAdapter(checkListAdapter);
+        }
 
 
         return view;
@@ -97,6 +102,11 @@ public class Sales_Fragment_Checklist extends Fragment implements AdapterView.On
 
     @Override
     public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+
+    @Override
+    public void onImageClick(String CheckedData, boolean isChecked) {
 
     }
 
